7e09ac31d6b1a31f670830fa5eadcf73609a1ad0
Gary Ranson <gary.ranson@clara.net>
2013-05-10 16:19:51 +0100

    Change 1

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
+  res.end('Hello World ta dah!\n');
 }).listen(port);
