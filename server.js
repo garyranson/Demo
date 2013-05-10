4ee5cbd7f3454e13c55ef4ba1c0a190b5f6099a7
Gary Ranson <gary.ranson@clara.net>
2013-05-10 17:32:10 +0100

    Azure 2

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+process.env.AZURE_SERVICEBUS_NAMESPACE= "Endpoint=sb://peractodev.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=F9BtX9MXwJYQ2vc4G+GbGeYHn5lrn7UOPVXPRxhMOVQ=";   
+process.env.AZURE_SERVICEBUS_ACCESS_KEY= "F9BtX9MXwJYQ2vc4G+GbGeYHn5lrn7UOPVXPRxhMOVQ=";
 var http = require('http');
 var azure = require('azure');
 
@@ -12,5 +14,5 @@ serviceBusService.createQueueIfNotExists('testqueue', function(error){
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World bingo\n');
+  res.end('Hello World bingo azure 2\n');
 }).listen(port);
