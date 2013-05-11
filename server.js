d3af3e6bb52e5029581abe212236831fb371ed07
Gary Ranson <gary.ranson@clara.net>
2013-05-11 17:42:30 +0100

    Storeage Queue

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
 process.env.AZURE_SERVICEBUS_NAMESPACE= "peractodev";   
 process.env.AZURE_SERVICEBUS_ACCESS_KEY= "F9BtX9MXwJYQ2vc4G+GbGeYHn5lrn7UOPVXPRxhMOVQ=";
+
+process.env.AZURE_STORAGE_ACCOUNT="peractodata";
+process.env.AZURE_STORAGE_ACCESS_KEY="on6JPdBRLixpYWToe+jZ/rZ4RmDcDEwPLt+0sZidws57ZzfzaOTrT1FKKunbaWiBs/aE9qJy0tj83rO4WoybPQ==";
+
 var http = require('http');
 var azure = require('azure');
 
+var queueService = azure.createQueueService();
+
+queueService.createQueueIfNotExists("testqueue", function(error){
+    if(!error){
+        // Queue exists
+    }
+});
+
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+
+	var body = "";
+	req.on('data', function (chunk) {
+		body += chunk;
+	});
+	req.on('end', function () {
+		var message = {
+			body: body,
+			customProperties: {requestUrl: req.url}
+		};
+		queueService.createMessage(queueName, message, function(error){
+			if(!error){
+				// Message inserted
+			}
+		});		
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
+		res.end('loaderio-493151bc95c1d6ef3d271b97e6823007');
+	});
+}).listen(port);
+
+
+/*
 var serviceBusService = azure.createServiceBusService();
 
 serviceBusService.createQueueIfNotExists('testqueue3', function(error){
@@ -11,7 +47,6 @@ serviceBusService.createQueueIfNotExists('testqueue3', function(error){
     }
 });
 
-var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
 
 	var body = "";
@@ -33,3 +68,4 @@ http.createServer(function(req, res) {
 		res.end('loaderio-493151bc95c1d6ef3d271b97e6823007');
 	});
 }).listen(port);
+*/
