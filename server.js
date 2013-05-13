b54a41b4ce52e01772f1bef7bd146b2c8afc30d8
Gary Ranson <gary.ranson@clara.net>
2013-05-13 08:34:42 +0100

    Storage Q 2

--- server.js
+++ server.js
@@ -4,34 +4,46 @@ process.env.AZURE_SERVICEBUS_ACCESS_KEY= "F9BtX9MXwJYQ2vc4G+GbGeYHn5lrn7UOPVXPRx
 process.env.AZURE_STORAGE_ACCOUNT="peractodata";
 process.env.AZURE_STORAGE_ACCESS_KEY="on6JPdBRLixpYWToe+jZ/rZ4RmDcDEwPLt+0sZidws57ZzfzaOTrT1FKKunbaWiBs/aE9qJy0tj83rO4WoybPQ==";
 
+console.log("Here1");
+
 var http = require('http');
 var azure = require('azure');
 
+console.log("Got requires");
+
 var queueService = azure.createQueueService();
+
+console.log("Created service");
+
 
 queueService.createQueueIfNotExists("testqueue", function(error){
     if(!error){
-        // Queue exists
+console.log("Queue already exists");
     }
 });
 
 var port = process.env.PORT || 1337;
+
 http.createServer(function(req, res) {
 
 	var body = "";
+console.log("New Request");
 	req.on('data', function (chunk) {
 		body += chunk;
+		console.log("Chunk");
 	});
 	req.on('end', function () {
 		var message = {
 			body: body,
 			customProperties: {requestUrl: req.url}
 		};
+		console.log("Sneding");
 		queueService.createMessage(queueName, message, function(error){
 			if(!error){
-				// Message inserted
+				console.log("Inserted");
 			}
 		});		
+		console.log("Response");
 		res.writeHead(200, { 'Content-Type': 'text/plain' });
 		res.end('loaderio-493151bc95c1d6ef3d271b97e6823007');
 	});
